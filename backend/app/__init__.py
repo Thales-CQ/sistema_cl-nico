44d0f7b068a088a5835a3cf03c0f6114f1f9a5f3
Thales-CQ <thalescq93@gmail.comclear>
2026-09-12 14:55:30 -0300

    feat: configura conexao do Flask com MySQL

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 
 from .config import Config
 from .routes import register_routes
-
+from .extensions import db, migrate
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     register_routes(app)
 
